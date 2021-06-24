d26ea08d3bf0e1dab2b424cb46fb384b2e769696
Mike Hall <mike@just3ws.com>
2021-06-24 12:04:43 -0500

    Make ubuntu user default

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,9 +5,9 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'malina101', user: 'deploy', roles: %w[web]
-server 'malina102', user: 'deploy', roles: %w[app]
-server 'malina103', user: 'deploy', roles: %w[db]
+server 'malina101', user: 'ubuntu', roles: %w[web]
+server 'malina102', user: 'ubuntu', roles: %w[app]
+server 'malina103', user: 'ubuntu', roles: %w[db]
 
 # role-based syntax
 # ==================
@@ -17,9 +17,9 @@ server 'malina103', user: 'deploy', roles: %w[db]
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
+# role :app, %w{ubuntu@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
+# role :db,  %w{ubuntu@example.com}
 
 # Configuration
 # =============
