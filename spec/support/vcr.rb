0e1a6bef62f3c5fbbb6d2278eba49373e96e6672
Mike Hall <mike@just3ws.com>
2021-06-27 19:29:58 -0500

    Foo

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,7 +4,7 @@ require 'vcr'
 
 VCR.configure do |c|
   c.allow_http_connections_when_no_cassette = false
-  c.cassette_library_dir = 'cassettes'
+  c.cassette_library_dir = 'spec/cassettes'
   c.configure_rspec_metadata!
   c.default_cassette_options = { record: :new_episodes }
   c.default_cassette_options = { serialize_with: :json }
