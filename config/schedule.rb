580528ace22442c549b4c8de43970d4b3c050898
Mike Hall <mike@just3ws.com>
2021-07-02 08:32:06 -0500

    Configuring whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,21 +1,28 @@
 # frozen_string_literal: true
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+require 'whenever'
 
-# Learn more: http://github.com/javan/whenever
+env 'MAILTO', 'mike@just3ws.com'
+
+set :job_template, "/usr/bin/env bash -l -c ':job'"
+set :output, '/home/ubuntu/outlier_jobs/shared/log/cron_production.log'
+
+JOB_PREFIX = ' cd :path && PATH=:env_path:"$PATH" RAILS_ENV=:environment nice -n 20 '
+
+job_type :exclusive_rails, " cd :path && flock -n 'tmp/pids/cron_:task.lock' -c 'PATH=:env_path:\"$PATH\" RAILS_ENV=:environment nice -n 20 bundle exec rails :task --silent --backtrace :output'"
+
+job_type :rails,  " #{JOB_PREFIX} bundle exec rails :task --silent :output "
+job_type :script, " #{JOB_PREFIX} bundle exec bin/:task :output "
+job_type :runner, " #{JOB_PREFIX} bin/rails runner -e :environment ':task' :output "
+
+# every('0 01 * * 0') { rails 'pghero:clean_query_stats' }
+# every('0 0-23 * * *') { rails 'pghero:capture_space_stats' }
+# every('*/15 * * * *') { rails 'pghero:capture_query_stats' }
+#
+# every('3 0/5 * * *') { rails 'outlierjobs:hackernews:process' }
+# every('3 0/7 * * *') { rails 'outlierjobs:stackoverflow:dedupe' }
+#
+# every('00 02,22 * * *') { exclusive_rails 'outlierjobs:hackernews:search' }
+# every('00 03,23 * * *') { exclusive_rails 'outlierjobs:stackoverflow:search' }
+#
+# every('3 0/11 * * *') { rails 'outlierjobs:stackoverflow:process' }
