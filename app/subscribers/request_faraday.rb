756b1544a70d59f28e785346629ac7ee8c6dc3fd
Mike Hall <mike@just3ws.com>
2021-07-03 12:17:08 -0500

    Notifications

--- app/subscribers/request_faraday.rb
+++ app/subscribers/request_faraday.rb
@@ -3,14 +3,16 @@
 ActiveSupport::Notifications.monotonic_subscribe('request.faraday') do |event| # |name, start, finish, id, env|
   puts '------------ notify >>>>>>>>>>>>'
 
-  puts "#{Rainbow('Payload:').bold.blue} #{event.payload.class}"
-  puts "#{Rainbow('Started:').bold.blue} #{event.time.class}"
-  puts "#{Rainbow('Finished:').bold.blue} #{event.end.class}"
-  puts "#{Rainbow('Duration (ms):').bold.blue} #{event.duration.class}"
-  puts "#{Rainbow('CPU time (ms):').bold.blue} #{event.cpu_time.class}"
-  puts "#{Rainbow('Idle time (ms):').bold.blue} #{event.idle_time.class}"
+  puts "#{Rainbow('Transaction ID:').bold.blue} #{event.transaction_id.class}"
   puts "#{Rainbow('Allocations:').bold.blue} #{event.allocations.class}"
-  puts "#{Rainbow('Event:').bold.blue} #{event.inspect}"
+  puts "#{Rainbow('CPU time (ms):').bold.blue} #{event.cpu_time.class}"
+  puts "#{Rainbow('Duration (ms):').bold.blue} #{event.duration.class}"
+  puts "#{Rainbow('Event:').bold.blue} #{event.as_json.keys}"
+  puts "#{Rainbow('Finished:').bold.blue} #{event.end.class}"
+  puts "#{Rainbow('Idle time (ms):').bold.blue} #{event.idle_time.class}"
+  puts "#{Rainbow('Payload:').bold.blue} #{event.payload.class}"
+  puts "#{Rainbow('Payload:').bold.blue} #{event.payload.as_json.keys}"
+  puts "#{Rainbow('Started:').bold.blue} #{event.time.class}"
 
   puts '<<<<<<<<<<<< notify ------------'
 end
