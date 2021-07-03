6dc219c310c67f7963ac1cf3a09fb83e2cedde2b
Mike Hall <mike@just3ws.com>
2021-07-03 12:09:30 -0500

    Adding subscription notifications

--- app/subscribers/request_faraday.rb
+++ app/subscribers/request_faraday.rb
@@ -3,13 +3,13 @@
 ActiveSupport::Notifications.monotonic_subscribe('request.faraday') do |event| # |name, start, finish, id, env|
   puts '------------ notify >>>>>>>>>>>>'
 
-  puts "#{Rainbow('Payload:').bold.blue} #{event.payload.body}"
-  puts "#{Rainbow('Started:').bold.blue} #{event.time}"
-  puts "#{Rainbow('Finished:').bold.blue} #{event.end}"
-  puts "#{Rainbow('Duration (ms):').bold.blue} #{event.duration}"
-  puts "#{Rainbow('CPU time (ms):').bold.blue} #{event.cpu_time}"
-  puts "#{Rainbow('Idle time (ms):').bold.blue} #{event.idle_time}"
-  puts "#{Rainbow('Allocations:').bold.blue} #{event.allocations}"
+  puts "#{Rainbow('Payload:').bold.blue} #{event.payload.class}"
+  puts "#{Rainbow('Started:').bold.blue} #{event.time.class}"
+  puts "#{Rainbow('Finished:').bold.blue} #{event.end.class}"
+  puts "#{Rainbow('Duration (ms):').bold.blue} #{event.duration.class}"
+  puts "#{Rainbow('CPU time (ms):').bold.blue} #{event.cpu_time.class}"
+  puts "#{Rainbow('Idle time (ms):').bold.blue} #{event.idle_time.class}"
+  puts "#{Rainbow('Allocations:').bold.blue} #{event.allocations.class}"
   puts "#{Rainbow('Event:').bold.blue} #{event.inspect}"
 
   puts '<<<<<<<<<<<< notify ------------'
