c861595725979792d5d6932a93e521d452fa516d
Mike Hall <mike@just3ws.com>
2021-07-04 15:17:19 -0500

    Carve out hour for Dice

--- config/schedule.rb
+++ config/schedule.rb
@@ -16,15 +16,24 @@ job_type :script, " #{JOB_PREFIX} bundle exec bin/:task :output "
 job_type :runner, " #{JOB_PREFIX} bin/rails runner -e :environment ':task' :output "
 job_type :command,  " #{JOB_PREFIX} :task :output "
 
-every('0 * * * *') { runner 'exe/stackoverflow' }
-every('8 * * * *') { runner 'exe/remotepython' }
-every('16 * * * *') { runner 'exe/nexxt' }
-every('24 * * * *') { runner 'exe/indeed' }
-every('32 * * * *') { runner 'exe/hackernews' }
-every('40 * * * *') { runner 'exe/remoteok' }
-every('48 * * * *') { runner 'exe/monster' }
-every('56 * * * *') { runner 'exe/weworkremotely' }
+every('0 0-7 * * *') { runner 'exe/stackoverflow' }
+every('8 0-7 * * *') { runner 'exe/remotepython' }
+every('16 0-7 * * *') { runner 'exe/nexxt' }
+every('24 0-7 * * *') { runner 'exe/indeed' }
+every('32 0-7 * * *') { runner 'exe/hackernews' }
+every('40 0-7 * * *') { runner 'exe/remoteok' }
+every('48 0-7 * * *') { runner 'exe/monster' }
+every('56 0-7 * * *') { runner 'exe/weworkremotely' }
 
-every :day, at: "6:#{rand(0..59)}am", roles: [:cron] do # UTC
+every :day, at: '8:08am', roles: [:cron] do # UTC
   command 'exe/dice'
 end
+
+every('0 9-23 * * *') { runner 'exe/stackoverflow' }
+every('8 9-23 * * *') { runner 'exe/remotepython' }
+every('16 9-23 * * *') { runner 'exe/nexxt' }
+every('24 9-23 * * *') { runner 'exe/indeed' }
+every('32 9-23 * * *') { runner 'exe/hackernews' }
+every('40 9-23 * * *') { runner 'exe/remoteok' }
+every('48 9-23 * * *') { runner 'exe/monster' }
+every('56 9-23 * * *') { runner 'exe/weworkremotely' }
