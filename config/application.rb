786397c0bbc32ccac291b8a4389b077564908713
Mike Hall <mike@just3ws.com>
2021-07-04 09:02:32 -0500

    Run as UTC

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,8 @@ module OutlierJobs
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'UTC'
+    config.active_record.default_timezone = :utc
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.api_only = true
