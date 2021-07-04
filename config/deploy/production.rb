d50b2ec61eaba62b48c06cb3437ae9f2704c60db
Mike Hall <mike@just3ws.com>
2021-07-04 13:07:30 -0500

    Added 104 to list

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,5 +4,6 @@ role :web, %w[malina101]
 role :app, %w[malina101 malina102]
 role :cron, %w[malina102]
 role :sidekiq, %w[malina102]
-role :redis, %w[malina102 malina101]
+role :redis, %w[malina101 malina102]
+role :files, %w[malina104]
 role :db, %w[malina103], primary: true
