9dc1444d1a190c1e576da11f8af7b940df6e12cd
Mike Hall <mike@just3ws.com>
2021-07-04 17:21:55 -0500

    What am i

--- app/models/notifications/request_faraday.rb
+++ app/models/notifications/request_faraday.rb
@@ -2,9 +2,18 @@
 
 module Notifications
   class RequestFaraday < ApplicationRecord
-    # jsonb_accessor :event, title: :string, external_id: :integer, reviewed_at: :datetime
+    # jsonb_accessor :event, end: :float, name: :string, time: :float, cpu_time_start: :float, transaction_id: :string, cpu_time_finish: :float, allocation_count_start: :integer, allocation_count_finish: :integer
+
+    def rss?
+      return false if payload.dig('response', 'body').blank?
+      return false if payload.dig('response', 'body').is_a?(Array)
+
+      payload.dig('response', 'body', 'rss', 'channel', 'item').present?
+    end
   end
 end
+
+# Notifications::RequestFaraday.last.payload.dig('response', 'body', 'rss', 'channel', 'item')
 
 # == Schema Information
 # Schema version: 20210703173154
