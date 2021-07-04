871f30964934bd4cb18995d3d9ab76629991966e
Mike Hall <mike@just3ws.com>
2021-07-03 19:01:32 -0500

    Added RemoteOK

---
+++ app/services/remoteok.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module RemoteOK
+  module_function
+
+  def jobs
+    service = RemoteOK::Feed.new(client: nil, params: nil)
+
+    service.call
+
+    service.data.dig('rss', 'channel', 'item')
+  end
+  # remote-dev-jobs.rss
+
+  def client
+    Faraday.new do |f|
+      f.request :retry, max: 3, interval: 0.05, interval_randomness: 0.5, backoff_factor: 3, max_interval: 900
+
+      f.headers[:user_agent] = 'OutlierJobs::RemoteOK/1.0'
+
+      f.ssl[:verify] = false
+
+      f.url_prefix = 'https://remoteok.io'
+
+      f.headers[:accept] = 'application/xml'
+
+      f.use :instrumentation
+      f.response :xml, content_type: /\bxml$/
+      f.response :encoding
+      f.response :follow_redirects
+
+      f.adapter :typhoeus
+    end
+  end
+end
--- config/schedule.rb
+++ config/schedule.rb
@@ -16,9 +16,11 @@ job_type :script, " #{JOB_PREFIX} bundle exec bin/:task :output "
 job_type :runner, " #{JOB_PREFIX} bin/rails runner -e :environment ':task' :output "
 
 every(36.minutes) { runner 'exe/hackernews' }
-every(37.minutes) { runner 'exe/stackoverflow' }
+every(37.minutes) { runner 'exe/remoteok' }
 every(38.minutes) { runner 'exe/monster' }
 every(39.minutes) { runner 'exe/weworkremotely' }
+
+every(1.hour) { runner 'exe/stackoverflow' }
 
 # every('0 01 * * 0') { rails 'pghero:clean_query_stats' }
 # every('0 0-23 * * *') { rails 'pghero:capture_space_stats' }
