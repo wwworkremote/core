dc6317c93309539d9c288a30f14938f658ef79a1
Mike Hall <mike@just3ws.com>
2021-07-05 16:49:11 -0500

    Cleanup

--- exe/patterns.rb
+++ exe/patterns.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env /Users/mike/projects/outlier_jobs/rails runner
 # frozen_string_literal: true
 
-
 feed = Notifications::RequestFaraday.find_each.flat_map do |notification|
   payload = notification.payload.deep_symbolize_keys
 
@@ -9,57 +8,94 @@ feed = Notifications::RequestFaraday.find_each.flat_map do |notification|
   in response_body: String | Array
     nil
 
-  in response_body: { by: String => by, id: Integer => id, text: String => text, title: String => title, type: String => type, time: Integer => time, score: Integer } if type == 'job'
+  in response_body: {
+    by: String => by,
+    id: Integer => id,
+    score: Integer,
+    text: String => text,
+    time: Integer => time,
+    title: String => title,
+    type: String => type
+  } if type == 'job'
 
     {
+      body: text.strip,
+      external_author_id: by,
       external_id: id,
       published_at: Time.at(time, in: 'UTC').utc,
-      title: title.strip,
-      body: text.strip,
-      external_author_id: by
+      title: title.strip
     }
-  in response_body: { by: String => by, id: Integer => id, url: String => url, time: Integer => time, type: String => type, score: Integer, title: String => title } if type == 'job'
+  in response_body: {
+    by: String => by,
+    id: Integer => id,
+    score: Integer,
+    time: Integer => time,
+    title: String => title,
+    type: String => type,
+    url: String => url
+  } if type == 'job'
 
     {
+      external_author_id: by,
       external_id: id,
       published_at: Time.at(time, in: 'UTC').utc,
       target_url: url,
-      title: title.strip,
-      external_author_id: by
+      title: title.strip
     }
 
-  in response_body: { by: String => by, id: Integer => id, url: String => url, time: Integer => time, type: String => type, score: Integer, title: String => title, text: String => text } if type == 'job'
+  in response_body: {
+    by: String => by,
+    id: Integer => id,
+    score: Integer,
+    text: String => text,
+    time: Integer => time,
+    title: String => title,
+    type: String => type,
+    url: String => url
+  } if type == 'job'
 
     {
+      body: text.strip,
+      external_author_id: by,
       external_id: id,
       published_at: Time.at(time, in: 'UTC').utc,
       target_url: url,
-      title: title.strip,
-      body: text.strip,
-      external_author_id: by
+      title: title.strip
     }
 
   # Filter for RSS job postings
   in response_body: { rss: { channel: { item: [*items] } } }
     items.flat_map do |item|
       case item
-      in { guid: { __content__: String => guid }, pubDate: String => pub_date, link: String => link, title: String => title, description: String => description }
+      in {
+        description: String => description,
+        guid: { __content__: String => guid },
+        link: String => link,
+        pubDate: String => pub_date,
+        title: String => title
+      }
 
         {
+          body: description.strip,
           external_id: guid,
           published_at: Time.strptime(pub_date, '%a, %d %b %Y %H:%M:%S %Z'),
           target_url: link,
-          title: title.strip,
-          body: description.strip
+          title: title.strip
         }
-      in { guid: String => guid, pubDate: String => pub_date, link: String => link, title: String => title, description: String => description }
+      in {
+        description: String => description,
+        guid: String => guid,
+        link: String => link,
+        pubDate: String => pub_date,
+        title: String => title
+      }
 
         {
+          body: description.strip,
           external_id: guid,
           published_at: Time.strptime(pub_date, '%a, %d %b %Y %H:%M:%S %Z'),
           target_url: link,
-          title: title,
-          body: description.strip
+          title: title
         }
       end
     end
