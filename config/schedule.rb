01631ce0c9787fc5be9b2944a7d40978d85e169e
Mike Hall <mike@just3ws.com>
2021-07-06 20:08:37 -0500

    Again

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,30 +18,26 @@ job_type :command,  " #{JOB_PREFIX} :task :output "
 
 FETCHERS = %w[hackernews indeed monster\ api monster\ bash monster\ css monster\ git monster\ github monster\ gitlab monster\ golang monster\ javascript monster\ jquery monster\ nodejs monster\ postgres monster\ postgresql monster\ python monster\ rails monster\ redis monster\ rspec monster\ ruby monster\ sass monster\ sidekiq monster\ sql monster\ vim monster\ zsh nexxt remoteok remotepython stackoverflow\ api stackoverflow\ bash stackoverflow\ css stackoverflow\ git stackoverflow\ github stackoverflow\ gitlab stackoverflow\ golang stackoverflow\ javascript stackoverflow\ jquery stackoverflow\ nodejs stackoverflow\ postgres stackoverflow\ postgresql stackoverflow\ python stackoverflow\ rails stackoverflow\ redis stackoverflow\ rspec stackoverflow\ ruby stackoverflow\ sass stackoverflow\ sidekiq stackoverflow\ sql stackoverflow\ vim stackoverflow\ zsh weworkremotely].freeze
 
-module Cronner
-  module_function
+def planner(slots:, duration:)
+  count_from = ((duration.to_f / slots.count) / 2).to_i
+  count_by = duration.to_f / (slots.count + 1)
 
-  def planner(slots:, duration:)
-    count_from = ((duration.to_f / slots.count) / 2).to_i
-    count_by = duration.to_f / (slots.count + 1)
+  count_from.step(duration, count_by).map(&:to_i).zip(slots)
+end
 
-    count_from.step(duration, count_by).map(&:to_i).zip(slots)
-  end
+def scheduler(start_at:, slots:)
+  slots.each do |slot|
+    time = start_at + slot.first
 
-  def scheduler(start_at:, slots:)
-    slots.each do |slot|
-      time = start_at + slot.first
+    cron = "#{time.min} #{time.hour} * * *"
+    task = slot.last
 
-      cron = "#{time.min} #{time.hour} * * *"
-      task = slot.last
+    every(cron) { runner "exe/#{task}" }
 
-      every(cron) { runner "exe/#{task}" }
-
-      # {
-      #   cron: "#{time.min} #{time.hour} * * *",
-      #   task: slot.last
-      # }
-    end
+    # {
+    #   cron: "#{time.min} #{time.hour} * * *",
+    #   task: slot.last
+    # }
   end
 end
 
@@ -56,16 +52,16 @@ end
 # end
 
 # start_at: Date.today.to_datetime.to_time.utc,
-Cronner.scheduler(
+scheduler(
   start_at: Date.today.to_datetime.to_time.utc,
-  slots: Cronner.planner(slots: FETCHERS, duration: 7.hours)
+  slots: planner(slots: FETCHERS, duration: 7.hours)
 )
 
 every :day, at: '8:08am', roles: [:cron] do # UTC
   command 'exe/dice'
 end
 
-Cronner.scheduler(
+scheduler(
   start_at: Date.today.to_datetime.to_time.utc + 8.hours,
-  slots: Cronner.planner(slots: FETCHERS, duration: 16.hours)
+  slots: planner(slots: FETCHERS, duration: 16.hours)
 )
