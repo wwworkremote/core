6ffcf35d62a75741dd4f1e2af011ed868f2472de
Mike Hall <mike@just3ws.com>
2021-07-07 15:15:27 -0500

    Handle empty results

--- exe/patterns.rb
+++ exe/patterns.rb
@@ -10,141 +10,96 @@ feed = query.find_each.flat_map do |notification|
 
   response_body = response_body.deep_symbolize_keys
 
-  case response_body
-  in response_body: String | Array
-    nil
+  begin
+    case response_body
+    in response_body: String | Array
+      nil
 
-  in by: String => by,
-      id: Integer => id,
-      score: Integer,
-      text: String => text,
-      time: Integer => time,
-      title: String => title,
-      type: String => type if type == 'job'
+    in by: String => by,
+    id: Integer => id,
+    score: Integer,
+    text: String => text,
+    time: Integer => time,
+    title: String => title,
+    type: String => type if type == 'job'
 
-    {
-      notification_id: notification.id,
-      body: text.strip,
-      external_author_id: by,
-      external_id: id.to_s.strip,
-      published_at: Time.at(time, in: 'UTC').utc,
-      title: title.strip
-    }
+      { notification_id: notification.id, body: text.strip, external_author_id: by, external_id: id.to_s.strip, published_at: Time.at(time, in: 'UTC').utc, title: title.strip }
 
-  in by: String => by,
-      id: Integer => id,
-      score: Integer,
-      time: Integer => time,
-      title: String => title,
-      type: String => type,
-      url: String => url if type == 'job'
+    in by: String => by,
+    id: Integer => id,
+    score: Integer,
+    time: Integer => time,
+    title: String => title,
+    type: String => type,
+    url: String => url if type == 'job'
 
-    {
-      notification_id: notification.id,
-      external_author_id: by,
-      external_id: id.to_s.strip,
-      published_at: Time.at(time, in: 'UTC').utc,
-      target_url: url,
-      title: title.strip
-    }
+      { notification_id: notification.id, external_author_id: by, external_id: id.to_s.strip, published_at: Time.at(time, in: 'UTC').utc, target_url: url, title: title.strip }
 
-  in by: String => by,
-      id: Integer => id,
-      score: Integer,
-      text: String => text,
-      time: Integer => time,
-      title: String => title,
-      type: String => type,
-      url: String => url if type == 'job'
+    in by: String => by,
+    id: Integer => id,
+    score: Integer,
+    text: String => text,
+    time: Integer => time,
+    title: String => title,
+    type: String => type,
+    url: String => url if type == 'job'
 
-    {
-      notification_id: notification.id,
-      body: text.strip,
-      external_author_id: by,
-      external_id: id.to_s.strip,
-      published_at: Time.at(time, in: 'UTC').utc,
-      target_url: url,
-      title: title.strip
-    }
+      { notification_id: notification.id, body: text.strip, external_author_id: by, external_id: id.to_s.strip, published_at: Time.at(time, in: 'UTC').utc, target_url: url, title: title.strip }
 
-  in rss: { channel: { item: [*items] } }
-    items.flat_map do |item|
-      case item
+    in rss: { channel: { item: [*items] } }
+      items.flat_map do |item|
+        case item
 
-      in company: String => company,
-          description: String => description,
-          guid: String => guid,
-          image: String,
-          link: String => link,
-          location: String => location,
-          pubDate: String => pub_date,
-          tags: String => tags,
-          title: String => title
+        in link: String, title: String => title if title.include?('No job postings found')
 
-        {
-          notification_id: notification.id,
-          body: description.strip,
-          external_id: guid.to_s.strip,
-          published_at: DateTime.parse(pub_date).to_time,
-          target_url: link,
-          title: title.strip,
-          location: location,
-          company: company,
-          tags: tags.split(',').map(&:strip)
-        }
+          nil
 
-      in company: String => company,
-          description: String => description,
-          guid: String => guid,
-          image: String,
-          link: String => link,
-          location:,
-          pubDate: String => pub_date,
-          tags: String => tags,
-          title: String => title
+        in company: String => company,
+        description: String => description,
+        guid: String => guid,
+        image: String,
+        link: String => link,
+        location: String => location,
+        pubDate: String => pub_date,
+        tags: String => tags,
+        title: String => title
 
-        {
-          notification_id: notification.id,
-          body: description.strip,
-          external_id: guid.to_s.strip,
-          published_at: DateTime.parse(pub_date).to_time,
-          target_url: link,
-          title: title.strip,
-          company: company,
-          tags: tags.split(',').map(&:strip)
-        }
+          { notification_id: notification.id, body: description.strip, external_id: guid.to_s.strip, published_at: DateTime.parse(pub_date).to_time, target_url: link, title: title.strip, location: location, company: company, tags: tags.split(',').map(&:strip) }
 
-      in description: String => description,
-          guid: { __content__: String => guid },
-          link: String => link,
-          pubDate: String => pub_date,
-          title: String => title
+        in company: String => company,
+        description: String => description,
+        guid: String => guid,
+        image: String,
+        link: String => link,
+        location:,
+        pubDate: String => pub_date,
+        tags: String => tags,
+        title: String => title
 
-        {
-          notification_id: notification.id,
-          body: description.strip,
-          external_id: guid.to_s.strip,
-          published_at: Time.strptime(pub_date, '%a, %d %b %Y %H:%M:%S %Z'),
-          target_url: link,
-          title: title.strip
-        }
+          { notification_id: notification.id, body: description.strip, external_id: guid.to_s.strip, published_at: DateTime.parse(pub_date).to_time, target_url: link, title: title.strip, company: company, tags: tags.split(',').map(&:strip) }
 
-      in description: String => description,
-          guid: String => guid,
-          link: String => link,
-          pubDate: String => pub_date,
-          title: String => title
+        in description: String => description,
+        guid: { __content__: String => guid },
+        link: String => link,
+        pubDate: String => pub_date,
+        title: String => title
 
-        {
-          notification_id: notification.id,
-          body: description.strip,
-          external_id: guid.to_s.strip,
-          published_at: Time.strptime(pub_date, '%a, %d %b %Y %H:%M:%S %Z'),
-          target_url: link,
-          title: title
-        }
+          { notification_id: notification.id, body: description.strip, external_id: guid.to_s.strip, published_at: Time.strptime(pub_date, '%a, %d %b %Y %H:%M:%S %Z'), target_url: link, title: title.strip }
+
+        in description: String => description,
+        guid: String => guid,
+        link: String => link,
+        pubDate: String => pub_date,
+        title: String => title
+
+          { notification_id: notification.id, body: description.strip, external_id: guid.to_s.strip, published_at: Time.strptime(pub_date, '%a, %d %b %Y %H:%M:%S %Z'), target_url: link, title: title }
+        end
+      rescue StandardError => e
+        ap [e.message, item]
       end
     end
+  rescue StandardError => e
+    ap [e.message, payload]
   end
 end
 
