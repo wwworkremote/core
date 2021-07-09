0112095f82b928234ea3ed4ba8281370362ec761
Mike Hall <mike@just3ws.com>
2021-07-09 17:40:17 -0500

    Sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  config.dsn = 'https://a9aaf54278e4413da92c138a53495caf@o522223.ingest.sentry.io/5647265'
+  config.dsn = 'https://fec1691c37fe4ba0bafe9f02118b1358@o522223.ingest.sentry.io/5858118'
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
   # Set tracesSampleRate to 1.0 to capture 100%
