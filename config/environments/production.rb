6af43d771ebc5180ce4115e8fcf36ff0797813a2
Mike Hall <mike@just3ws.com>
2021-07-10 18:13:59 -0500

    Write to syslog

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,14 +45,8 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Use a different logger for distributed setups.
-  # require "syslog/logger"
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-
-  # if ENV['RAILS_LOG_TO_STDOUT'].present?
-  #   logger = ActiveSupport::Logger.new($stdout)
-  #   logger.formatter = config.log_formatter
-  #   config.logger = ActiveSupport::TaggedLogging.new(logger)
-  # end
+  require 'syslog/logger'
+  config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new('outlier_jobs'))
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
