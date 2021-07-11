53b5526094543c93fff30ce980af7ccfae8bd43f
Mike Hall <mike@just3ws.com>
2021-07-11 15:43:02 -0500

    Env

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,6 +3,8 @@
 Sentry.init do |config|
   config.dsn = 'https://fec1691c37fe4ba0bafe9f02118b1358@o522223.ingest.sentry.io/5858118'
 
+  config.environment = 'production'
+
   config.send_default_pii = true
 
   config.breadcrumbs_logger = %i[sentry_logger active_support_logger http_logger]
