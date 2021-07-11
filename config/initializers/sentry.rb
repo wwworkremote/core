d523d0ff05ea7d8e1d7c3f806bd27cd87cadf66e
Mike Hall <mike@just3ws.com>
2021-07-11 11:11:53 -0500

    Added sentry config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,14 +2,24 @@
 
 Sentry.init do |config|
   config.dsn = 'https://fec1691c37fe4ba0bafe9f02118b1358@o522223.ingest.sentry.io/5858118'
-  config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
+  config.send_default_pii = true
+
+  config.breadcrumbs_logger = %i[sentry_logger active_support_logger http_logger]
+
+  config.enabled_environments = %w[production]
+  #
+  # config.logger = Sentry::Logger.new(STDOUT)
+  #
+  # config.inspect_exception_causes_for_exclusion = false
+  # config.excluded_exceptions += ['ActionController::RoutingError', 'ActiveRecord::RecordNotFound']
+  #
   # Set tracesSampleRate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production
-  config.traces_sample_rate = 0.5
-  # or
-  config.traces_sampler = lambda do |context|
-    true
-  end
+  config.traces_sample_rate = 1.0
+  # # or
+  # config.traces_sampler = lambda do |context|
+  #   true
+  # end
 end
