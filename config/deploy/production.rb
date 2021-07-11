01ee9d7dc57a1a0e454d00d31eb50f1e43313d0f
Mike Hall <mike@just3ws.com>
2021-07-11 14:25:00 -0500

    Disable malina104 deploy for now

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,5 +6,5 @@ role :cron1, %w[malina101]
 role :cron2, %w[malina102]
 role :sidekiq, %w[malina102]
 role :redis, %w[malina101 malina102]
-role :files, %w[malina104]
+# role :files, %w[malina104]
 role :db, %w[malina103], primary: true
