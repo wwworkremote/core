19982eec6c3a3e50abed7b7aff31580f987b4eed
Mike Hall <mike@just3ws.com>
2021-07-13 11:01:04 -0500

    Deploy to 104

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@ role :web, %w[malina101]
 role :app, %w[malina101 malina102]
 role :cron1, %w[malina101]
 role :cron2, %w[malina102]
+role :cron4, %w[malina104]
 role :sidekiq, %w[malina102]
 role :redis, %w[malina101 malina102]
-# role :files, %w[malina104]
 role :db, %w[malina103], primary: true
 
 set :sentry_api_token, '0e29254fac624501bae3c56d405d07b26863e9f1485b44c3aa5b12226d5157b7'
