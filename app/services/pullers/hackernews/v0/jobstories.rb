8715bbf0f8ceadfbd74e5c4feee1a7d2a6840fe8
Mike Hall <mike@just3ws.com>
2021-07-24 14:56:11 -0500

    Adds namespace

---
+++ app/services/pullers/hackernews/v0/jobstories.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Pullers
+  module HackerNews
+    module V0
+      class Jobstories
+        attr_reader :client
+
+        def initialize(client: nil)
+          @client = client || HackerNews.client
+        end
+
+        def call
+          request
+          self
+        end
+
+        def request
+          @request ||= client.get('jobstories.json')
+        end
+
+        def data
+          @data ||= request.body
+        end
+      end
+    end
+  end
+end
---
+++ app/services/pullers/stackoverflow/jobs.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Pullers
+  module StackOverflow
+    module Jobs
+    end
+  end
+end
