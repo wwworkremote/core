1312846c4ea13dd17ab0ac5125bdebeb7e2891bf
Mike Hall <mike@just3ws.com>
2021-07-30 13:07:38 -0500

    Foo

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-role :web, %w[malina101]
+# role :web, %w[malina101]
 role :app, %w[malina101 malina102]
-role :cron1, %w[malina101]
-role :cron2, %w[malina102]
+# role :cron1, %w[malina101]
+# role :cron2, %w[malina102]
 # role :cron4, %w[malina104]
-role :sidekiq, %w[malina102]
+# role :sidekiq, %w[malina102]
 role :redis, %w[malina101 malina102]
-role :db, %w[malina103], primary: true
+# role :db, %w[malina103], primary: true
