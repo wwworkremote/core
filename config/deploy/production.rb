3ca7140127111f40e89d53729802b83e89db4cc4
Mike Hall <mike@just3ws.com>
2021-07-30 15:37:16 -0500

    Restore 103

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,4 +7,4 @@ role :app, %w[malina101 malina102]
 # role :cron4, %w[malina104]
 # role :sidekiq, %w[malina102]
 role :redis, %w[malina101 malina102]
-# role :db, %w[malina103], primary: true
+role :db, %w[malina103], primary: true
