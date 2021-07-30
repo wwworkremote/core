c81c411ae4adc9880af2e8d4c9a6c47d4b4048a2
Mike Hall <mike@just3ws.com>
2021-07-30 16:33:37 -0500

    Whenever

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,8 +2,8 @@
 
 # role :web, %w[malina101]
 role :app, %w[malina101 malina102]
-# role :cron1, %w[malina101]
-# role :cron2, %w[malina102]
+role :cron1, %w[malina101]
+role :cron2, %w[malina102]
 # role :cron4, %w[malina104]
 # role :sidekiq, %w[malina102]
 role :redis, %w[malina101 malina102]
