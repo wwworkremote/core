b8c340a7ce4e0b023d74827f796c5cd85202ee9c
Mike Hall <mike@just3ws.com>
2021-08-01 18:21:07 -0500

    Fooooor

---
+++ spec/lib/pullers/nexxt_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+module Pullers
+  RSpec.describe Nexxt, vcr: true do
+    describe '.jobs' do
+      it do
+        ap described_class.jobs
+      end
+    end
+
+    describe '.client' do
+    end
+  end
+end
---
+++ spec/lib/pullers/weworkremotely_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+module Pullers
+  RSpec.describe WeWorkRemotely, vcr: true do
+    describe '.jobs' do
+      it do
+        ap described_class.jobs
+      end
+    end
+
+    describe '.client' do
+    end
+  end
+end
