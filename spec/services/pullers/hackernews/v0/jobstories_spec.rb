c2128abf13dbe24bce91a77af11fdda187198e09
Mike Hall <mike@just3ws.com>
2021-08-01 14:06:49 -0500

    Foobar

---
+++ spec/services/pullers/hackernews/v0/jobstories_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+module Pullers 
+  module HackerNews
+    module V0
+      RSpec.describe Jobstories do
+        it 'requests a list of jobstories', :vcr do
+          puts described_class.new.call.inspect
+        end
+      end
+    end
+  end
+end
