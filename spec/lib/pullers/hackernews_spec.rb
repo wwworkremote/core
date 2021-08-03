c92c6530a1e44f218032d223110c6fe4d142b915
Mike Hall <mike@just3ws.com>
2021-08-03 16:08:50 -0500

    fooorter

--- spec/lib/pullers/hackernews_spec.rb
+++ spec/lib/pullers/hackernews_spec.rb
@@ -5,12 +5,20 @@ require 'rails_helper'
 module Pullers
   RSpec.describe HackerNews, vcr: true do
     describe '.jobs' do
-      it do
-        ap described_class.jobs
-      end
-    end
+      subject(:jobs) { described_class.jobs }
 
-    describe '.client' do
+      before { jobs }
+
+      it { is_expected.to be_an(Array) }
+      it { is_expected.not_to be_empty }
+      it { is_expected.to all(be_a(Hash)) }
+
+      it { is_expected.to all(include('by')) }
+      it { is_expected.to all(include('id')) }
+      it { is_expected.to all(include('score')) }
+      it { is_expected.to all(include('time')) }
+      it { is_expected.to all(include('title')) }
+      it { is_expected.to all(include('type')) }
     end
   end
 end
