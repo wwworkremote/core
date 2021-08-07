867f3839ac0a20860bd418e2a0bd0c91a0554dd2
Mike Hall <mike@just3ws.com>
2021-08-07 18:38:31 -0500

    Push

--- db/migrate/20210807232217_add_suggested_indexes.rb
+++ db/migrate/20210807232217_add_suggested_indexes.rb
@@ -2,7 +2,7 @@
 
 class AddSuggestedIndexes < ActiveRecord::Migration[6.1]
   def change
-    # commit_db_transaction
-    # add_index :messages, [:created_at], algorithm: :concurrently
+    commit_db_transaction
+    add_index :messages, [:created_at], algorithm: :concurrently
   end
 end
