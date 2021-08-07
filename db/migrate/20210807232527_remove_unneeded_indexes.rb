5015d1efc144cf82a0297da14ae2e520e2d00ca3
Mike Hall <mike@just3ws.com>
2021-08-07 18:37:16 -0500

    Another'

--- db/migrate/20210807232527_remove_unneeded_indexes.rb
+++ db/migrate/20210807232527_remove_unneeded_indexes.rb
@@ -2,8 +2,8 @@
 
 class RemoveUnneededIndexes < ActiveRecord::Migration[6.1]
   def change
-    remove_index :friendly_id_slugs,
-                 name: 'index_friendly_id_slugs_on_slug_and_sluggable_type',
-                 column: %i[slug sluggable_type]
+    # remove_index :friendly_id_slugs,
+    #              name: 'index_friendly_id_slugs_on_slug_and_sluggable_type',
+    #              column: %i[slug sluggable_type]
   end
 end
