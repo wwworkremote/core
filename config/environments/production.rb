c6aa6dbf3558a806a4773a5c529770e0e3f7bad3
Mike Hall <mike@just3ws.com>
2021-08-08 14:13:55 -0500

    Redis cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,8 +31,17 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
-  config.cache_store = :redis_cache_store, { url: ENV.fetch('OUTLIER_JOBS_REDIS_CACHE_URL', 'redis://malina101:6379'), driver: :hiredis, namespace: 'out' }
+  config.cache_store = :redis_cache_store, {
+    connect_timeout: 30, # Defaults to 20 seconds
+    driver: :hiredis,
+    namespace: 'oj',
+    pool_size: 5,
+    pool_timeout: 5,
+    read_timeout: 0.2, # Defaults to 1 second
+    reconnect_attempts: 3, # Defaults to 0
+    url: 'redis://localhost:6379',
+    write_timeout: 0.2 # Defaults to 1 second
+  }
 
   config.i18n.fallbacks = true
 
