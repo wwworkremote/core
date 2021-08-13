e11a00afb908b311586e06b3b6b27c2664cc037a
Mike Hall <mike@just3ws.com>
2021-08-13 18:54:53 -0500

    Fix requires

--- config/initializers/01_redis.rb
+++ config/initializers/01_redis.rb
@@ -4,10 +4,4 @@ require 'redis'
 require 'hiredis'
 require 'redis/connection/hiredis'
 
-REDIS_URL = if Rails.env.production?
-              ENV['REDIS_URL'].presence || 'redis://localhost:6379'
-            else
-              'redis://localhost:6379'
-            end
-
-REDIS = Redis.new(url: REDIS_URL, driver: :hiredis)
+REDIS = Redis.new(url: 'redis://localhost:6379', driver: :hiredis)
