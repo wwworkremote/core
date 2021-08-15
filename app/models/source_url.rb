6c4f20a41172011f292d6c94eb39c1a6a1e10c2e
Mike Hall <mike@just3ws.com>
2021-08-15 14:21:58 -0500

    Source Urls

---
+++ app/models/source_url.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class SourceUrl < ApplicationRecord
+  belongs_to :source
+end
---
+++ db/migrate/20210815191413_create_source_urls.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class CreateSourceUrls < ActiveRecord::Migration[6.1]
+  def change
+    create_table :source_urls do |t|
+      t.string :url, null: false, uniq: true
+      t.string :protocol
+      t.string :host
+      t.string :path
+
+      t.jsonb :querystring, default: {}, null: false
+
+      t.datetime :created_at, precision: 6, default: -> { 'CURRENT_TIMESTAMP' }, null: false
+      t.datetime :updated_at, precision: 6, default: -> { 'CURRENT_TIMESTAMP' }, null: false
+    end
+  end
+end
