1055a121f943ede254daece4b868e65c1a462231
Mike Hall <mike@just3ws.com>
2021-08-29 19:24:03 -0500

    mv

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :whenever_roles, %i[cron1 cron2]
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+append :linked_files, 'config/database.yml', 'config/puma.rb', 'config/master.key', 'config/credentials.yml.enc'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
