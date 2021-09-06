43e31d89efc683baee74e8500c4a1efb68219ac8
Mike Hall <mike@just3ws.com>
2021-09-06 11:42:56 -0500

    Adding source url creation

--- app/models/source_url.rb
+++ app/models/source_url.rb
@@ -1,7 +1,28 @@
 # frozen_string_literal: true
 
 class SourceUrl < ApplicationRecord
-  belongs_to :source
+  belongs_to :source, optional: true
+
+  def self.create_by_uri(uri)
+    create(
+      url: uri.to_s,
+      host: uri.host,
+      protocol: uri.scheme,
+      querystring: extract_querystring(uri),
+      path: uri.path
+    )
+  end
+
+  def self.extract_querystring(uri)
+    return unless uri&.query
+
+    uri
+      .query
+      .split('&')
+      .map { |kv| kv.split('=') }
+      .each_with_object({}) { |kv, h| h[kv.first] = kv.last }
+      .deep_sort
+  end
 end
 
 # == Schema Information
