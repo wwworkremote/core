c92acd9cbfcb9559dc5c33e740039fd81974386c
Mike Hall <mike@just3ws.com>
2021-09-06 19:13:29 -0500

    Normalization

--- app/actions/normalize.rb
+++ app/actions/normalize.rb
@@ -36,7 +36,10 @@ class Normalize
   def self.normalize(text)
     return if text.blank?
 
-    ActionController::Base \
+    I18n.locale = :en
+
+    I18n.transliterate(
+      ActionController::Base
       .helpers
       .strip_tags(Sanitize.fragment(RubyPants.new(text.strip.scrub.downcase, stupefy: true).to_html))
       .unicode_normalize(:nfkc)
@@ -52,22 +55,47 @@ class Normalize
       .tr('“', '"')
       .tr('”', '"')
       .tr('', '')
-      .strip
+      .gsub(/aren't/, 'are not')
+      .gsub(/can't/, 'cannot')
+      .gsub(/didn't/, 'did not')
+      .gsub(/doesn't/, 'does not')
+      .gsub(/don't/, 'do not')
+      .gsub(/hasn't/, 'has not')
+      .gsub(/haven't/, 'have not')
+      .gsub(/he's/, 'he is')
+      .gsub(/i'll/, 'i will')
+      .gsub(/i'm/, 'i am')
+      .gsub(/i've/, 'i have')
+      .gsub(/isn't/, 'is not')
+      .gsub(/it'd/, 'it would')
+      .gsub(/it'll/, 'it will')
+      .gsub(/it's/, 'it is')
+      .gsub(/let's/, 'let us')
+      .gsub(/req'd/, 'required')
+      .gsub(/s'more/, 'some more')
+      .gsub(/shouldn't/, 'should not')
+      .gsub(/that'd/, 'that would')
+      .gsub(/that's/, 'that is')
+      .gsub(/there's/, 'there is')
+      .gsub(/they'll/, 'they will')
+      .gsub(/they're/, 'they are')
+      .gsub(/they've/, 'they have')
+      .gsub(/wasn't/, 'was not')
+      .gsub(/we'd/, 'we would')
+      .gsub(/we'll/, 'we will')
+      .gsub(/we're/, 'we are')
+      .gsub(/we've/, 'we have')
+      .gsub(/what's/, 'what is')
+      .gsub(/who's/, 'who is')
+      .gsub(/won't/, 'will not')
+      .gsub(/wouldn't/, 'would not')
+      .gsub(/ya'll/, 'you all')
+      .gsub(/you'd/, 'you would')
+      .gsub(/you'll/, 'you will')
+      .gsub(/you're/, 'you are')
+      .gsub(/you've/, 'you have')
+      .strip,
+      locale: :en
+    )
   end
 end
-
-# .gsub(/(θ|ð)/, 'th')
-# .gsub(/@/, '@')
-# .gsub(/^[[:print:]]/, '')
-# .gsub(/résumé/, 'resume')
-# .gsub(/‍/, '')
-# .localize.transliterate_into(:en)
-# .scrub
-# .tr("ü", 'u'),
-# .tr('`’“”—–', '\'\'""--')
-# .tr('ú', 'u')
-# .tr('ü', 'u')
-# .tr('', '')
-# .unicode_normalize(:nfkc)
-# text.downcase.strip.force_encoding('UTF-8').unicode_normalize(:nfkc),
-# text.downcase.strip.force_encoding('UTF-8').unicode_normalize(:nfkc).localize.transliterate_into(:en),
