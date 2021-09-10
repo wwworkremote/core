e4104c0ab3d39f48af7906db601c3875a161e76e
Mike Hall <mike@just3ws.com>
2021-09-10 17:57:03 -0500

    Paths

--- config/application.rb
+++ config/application.rb
@@ -29,15 +29,14 @@ module OutlierJobs
     config.active_record.default_timezone = :utc
     config.active_record.schema_format = :sql
 
-    # config.eager_load_paths << Rails.root.join('app/lib')
-    # config.autoload_paths << Rails.root.join('app/actions').to_s
-    # config.autoload_paths << Rails.root.join('app/lib').to_s
+    config.autoload_paths << Rails.root.join('app/actions').to_s
+    config.autoload_paths << Rails.root.join('app/lib').to_s
 
-    # config.eager_load_paths << Rails.root.join('app/actions').to_s
-    # config.eager_load_paths << Rails.root.join('app/lib').to_s
+    config.eager_load_paths << Rails.root.join('app/actions').to_s
+    config.eager_load_paths << Rails.root.join('app/lib').to_s
 
-    config.paths.add 'lib', eager_load: true
-    config.paths.add 'actions', eager_load: true
+    # config.paths.add 'lib', eager_load: true
+    # config.paths.add 'actions', eager_load: true
 
     config.api_only = true
   end
