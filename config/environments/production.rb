a026eeb9d1e37ff54b6d3804499597118e26c672
Mike Hall <mike@just3ws.com>
2021-09-16 16:35:51 -0500

    Stdout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,12 +59,6 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new('outlier_jobs'))
 
-  # if ENV['RAILS_LOG_TO_STDOUT'].present?
-  #   logger = ActiveSupport::Logger.new($stdout)
-  #   logger.formatter = config.log_formatter
-  #   config.logger = ActiveSupport::TaggedLogging.new(logger)
-  # end
-
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
@@ -88,4 +82,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  logger = ActiveSupport::Logger.new($stdout)
+  logger.formatter = config.log_formatter
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
 end
