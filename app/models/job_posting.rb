fc54ea52698c384e3b79dba3c3edeea7791d2927
Mike Hall <mike@just3ws.com>
2021-09-16 10:57:17 -0500

    Status nah

--- app/models/job_posting.rb
+++ app/models/job_posting.rb
@@ -1,21 +1,12 @@
 # frozen_string_literal: true
 
 class JobPosting < ApplicationRecord
-  enum status: {
-    pending: 0,
-    processed: 1
-  }, _prefix: true
-
   belongs_to :source, optional: true
 
   before_validation :sign, on: :create
   after_commit :update_source, on: :create
 
   validates :signature, presence: true
-
-  def update_source
-    source.status_processed! unless source.status_processed?
-  end
 
   private
 
