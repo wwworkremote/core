cd80da7d6d2c38603dd08c0e173af70d11e34f63
Mike Hall <mike@just3ws.com>
2021-09-16 16:30:40 -0500

    Added third cron app

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,16 +1,13 @@
 # frozen_string_literal: true
 
-# role :web, %w[malina101]
 role :web, %w[jagodka105]
+
 role :app, %w[malina101 malina102 malina103]
 
 role :cron1, %w[malina101]
 role :cron2, %w[malina102]
+role :cron3, %w[malina103]
 
-# role :cron4, %w[malina104]
-
-# role :sidekiq, %w[malina102]
-#
 role :redis, %w[malina101 malina102 malina103]
 
 role :db, %w[malina108], primary: true
