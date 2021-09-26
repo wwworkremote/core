e263ac659742fa7ce4feeda8d52fde6af0d3a2b6
Mike Hall <mike@just3ws.com>
2021-09-26 14:46:38 -0500

    Added domain model

---
+++ app/models/domain.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Domain < ApplicationRecord
+end
+
+# == Schema Information
+#
+# Table name: domains
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
