d36508983aa435452ef813fe12e6d69241a86211
Mike Hall <mike@just3ws.com>
2021-10-09 12:47:53 -0500

    Updating deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,8 +4,11 @@ role :web, %w[jagodka105]
 
 role :app, %w[malina101 malina102 malina103]
 
+role :cron, %w[malina101 malina102]
+
 role :cron1, %w[malina101]
 role :cron2, %w[malina102]
+
 role :sidekiq, %w[malina103]
 
 role :redis, %w[jagodka105 malina101 malina102 malina103]
