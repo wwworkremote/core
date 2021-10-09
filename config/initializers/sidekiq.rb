74ea4fd556767dc379cdc0e8d5d6ff7531b71815
Mike Hall <mike@just3ws.com>
2021-10-09 13:58:21 -0500

    Fixed config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,27 +1,25 @@
 # frozen_string_literal: true
 
-require_relative 'redis'
-
 require 'sidekiq'
 require 'sidekiq/api'
 
-# Sidekiq.default_worker_options = {
-#   backtrace: true,
-#   retry: false # No retries, just dead. Use `false` to skip dead queue.
-# }
+Sidekiq.default_worker_options = {
+  backtrace: true,
+  retry: false # No retries, just dead. Use `false` to skip dead queue.
+}
 
-# Sidekiq.configure_client do |config|
-#   config.redis = {
-#     url: 'redis://malina103:6379',
-#     driver: :hiredis,
-#     network_timeout: 10
-#   }
-# end
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: 'redis://malina103:6379',
+    driver: :hiredis,
+    network_timeout: 10
+  }
+end
 
-# Sidekiq.configure_server do |config|
-#   config.redis = {
-#     url: 'redis://malina103:6379',
-#     driver: :hiredis,
-#     network_timeout: 10
-#   }
-# end
+Sidekiq.configure_server do |config|
+  config.redis = {
+    url: 'redis://malina103:6379',
+    driver: :hiredis,
+    network_timeout: 10
+  }
+end
