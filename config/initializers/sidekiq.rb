2e191df8f809761de272742ec2a99ac770d8abcb
Mike Hall <mike@just3ws.com>
2021-10-09 15:36:29 -0500

    Adding async workers

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,9 +8,12 @@ Sidekiq.default_worker_options = {
   retry: false # No retries, just dead. Use `false` to skip dead queue.
 }
 
+sidekiq_redis_url = 'redis://localhost:6379'
+sidekiq_redis_url = 'redis://malina103:6379' if Rails.env.production?
+
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: 'redis://malina103:6379',
+    url: sidekiq_redis_url,
     driver: :hiredis,
     network_timeout: 10
   }
@@ -18,7 +21,7 @@ end
 
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: 'redis://malina103:6379',
+    url: sidekiq_redis_url,
     driver: :hiredis,
     network_timeout: 10
   }
