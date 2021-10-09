84a2fee2049fb2e165942373effd4d2b7c330230
Mike Hall <mike@just3ws.com>
2021-10-09 13:52:20 -0500

    Sidekiq

--- config/initializers/01_redis.rb
+++ config/initializers/01_redis.rb
@@ -5,3 +5,4 @@ require 'hiredis'
 require 'redis/connection/hiredis'
 
 REDIS = Redis.new(url: 'redis://localhost:6379', driver: :hiredis)
+SIDEKIQ_REDIS = Redis.new(url: 'redis://192.168.0.103:6379', driver: :hiredis)
