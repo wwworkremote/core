a050ee3dc64ca21c93d9ac3153e80dbf15e89282
Mike Hall <mike@just3ws.com>
2021-10-13 18:46:36 -0500

    Throttle jobs on specific queues

--- app/lib/runners/hackernews/async.rb
+++ app/lib/runners/hackernews/async.rb
@@ -4,11 +4,12 @@ module Runners
   module HackerNews
     class Async
       include Sidekiq::Worker
-      sidekiq_options queue: :runners
+      include Sidekiq::Throttled::Worker
+      sidekiq_options queue: :hackernews_runners
+      sidekiq_throttled(concurrency: { limit: 1 }, threshold: { limit: 1, period: 30.minutes })
 
       def perform(term = nil)
         term = 'sidekiq' if term.blank?
-        #         Rails.logger = Rails.logger.child(term: term, entrypoint: self.class.name)
         Runners::HackerNews.run(term: term)
       end
     end
