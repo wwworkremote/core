8570a20f73a2901d63c3c3df5d733749467af7d3
Mike Hall <mike@just3ws.com>
2021-10-15 10:56:53 -0500

    key_suffix

--- app/lib/runners/nexxt/async.rb
+++ app/lib/runners/nexxt/async.rb
@@ -11,16 +11,16 @@ module Runners
       sidekiq_throttle(
         concurrency: {
           limit: 1,
-          key_suffix: ->(term) { term }
+          key_suffix: ->(term:) { term.to_s }
         },
         threshold: {
           limit: 1,
           period: 15.minutes,
-          key_suffix: ->(term) { term }
+          key_suffix: ->(term:) { term.to_s }
         }
       )
 
-      def perform(term:)
+      def perform(term: 'sidekiq')
         Runners::Nexxt.run(term: term)
       end
     end
