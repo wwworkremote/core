6371767a1f64cca0931ba8b1a78f3651dfdb6799
Mike Hall <mike@just3ws.com>
2021-10-29 19:35:01 -0500

    AuthenticJobs

--- app/lib/pullers/monster/feed.rb
+++ app/lib/pullers/monster/feed.rb
@@ -7,7 +7,7 @@ module Pullers
 
       def initialize(client: nil, params: nil, path: nil, term: nil)
         @client = client || Monster.client
-        @params = params || { q: term.presence || 'ruby' }
+        @params = (params || { q: term.presence || 'ruby' }).compact_blank
         @path = path.to_s.strip
       end
 
