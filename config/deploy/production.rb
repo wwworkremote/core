8ac5947adb64b0dde8f7a0feb548845ac8bb6474
Mike Hall <mike@just3ws.com>
2021-11-20 16:08:53 -0600

    And jagodka

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,7 +9,7 @@ role :cron, %w[malina101 malina102]
 role :cron1, %w[malina101]
 role :cron2, %w[malina102]
 
-role :sidekiq, %w[malina103]
+role :sidekiq, %w[malina103 jagodka105]
 
 role :redis, %w[jagodka105 malina101 malina102 malina103]
 
