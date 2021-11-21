874a22fadcc7e2d0a6aaf9f1b32deb4aebba0bc1
Mike Hall <mike@just3ws.com>
2021-11-21 14:00:09 -0600

    Switching around

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,14 +2,14 @@
 
 role :web, %w[jagodka105]
 
-role :app, %w[malina101 malina102 malina103]
+role :app, %w[malina101 malina102 malina103 jagodka105]
 
 role :cron, %w[malina101 malina102]
 
 role :cron1, %w[malina101]
 role :cron2, %w[malina102]
 
-role :sidekiq, %w[malina103 jagodka105]
+role :sidekiq, %w[jagodka105]
 
 role :redis, %w[jagodka105 malina101 malina102 malina103]
 
