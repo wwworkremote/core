348628c7ecc9cb7ac5e6a5a054df023321bd5877
Mike Hall <mike@just3ws.com>
2021-12-26 12:59:59 -0600

    boop

---
+++ config/initializers/sequel.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+Sequel.extension(:symbol_as)
+
+PGDATABASE = Sequel.connect(
+  adapter: 'postgres',
+  host: 'localhost',
+  database: "wwworkremote_#{Rails.env}"
+)
+
+PGDATABASE.extension(:pg_streaming)
+
+PGDATABASE.stream_all_queries = true
+
+__END__
+# Sequel.connect (
+  after_connect: proc { |*args| puts [:after_connect, args].inspect },
+  before_preconnect: proc { |*args| puts [:after_connect, args].inspect },
+  notice_receiver: proc { |*args| puts [:notice_receiver, args].inspect },
+  single_threaded: true
+)
+
