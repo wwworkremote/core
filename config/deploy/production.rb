bbab7e3c1e85cce5820ed40d62fa2617c0e9b505
Mike Hall <mike@just3ws.com>
2022-06-05 12:29:55 -0500

    Cleanup

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,14 +5,6 @@
 NODES = %w[node01 node02 node03 node04 node05].freeze
 
 role :app, NODES
-
 role :cron, NODES
-
-# role :cron1, %w[node101]
-# role :cron2, %w[node102]
-
-role :sidekiq, NODES
-
-role :redis, NODES
-
 role :db, NODES
+role :redis, NODES
--- config/initializers/01_redis.rb
+++ config/initializers/01_redis.rb
@@ -5,4 +5,3 @@ require 'hiredis'
 require 'redis/connection/hiredis'
 
 REDIS = Redis.new(url: 'redis://localhost:6379', driver: :hiredis)
-SIDEKIQ_REDIS = Redis.new(url: 'redis://192.168.0.103:6379', driver: :hiredis)
