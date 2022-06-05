7a774a1c5ae05f49ae194d4fb0f4dd601d6332a0
Mike Hall <mike@just3ws.com>
2022-06-04 19:19:34 -0500

    Foobar

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
 
-Sequel.extension(:symbol_as)
-
-PGDATABASE = Sequel.connect(
-  adapter: 'postgres',
-  host: 'localhost',
-  database: "wwworkremote_#{Rails.env}"
-)
-
-PGDATABASE.extension(:pg_streaming)
-
-PGDATABASE.stream_all_queries = true
+# Sequel.extension(:symbol_as)
+#
+# PGDATABASE = Sequel.connect(
+#   adapter: 'postgres',
+#   host: 'localhost',
+#   database: "wwworkremote_#{Rails.env}"
+# )
+#
+# PGDATABASE.extension(:pg_streaming)
+#
+# PGDATABASE.stream_all_queries = true
 
 __END__
+
 # Sequel.connect (
   after_connect: proc { |*args| puts [:after_connect, args].inspect },
   before_preconnect: proc { |*args| puts [:after_connect, args].inspect },
