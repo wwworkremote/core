352928cee1dc53da41da4d528ba2f0706b66c75a
Mike Hall <mike@just3ws.com>
2022-06-04 19:25:21 -0500

    No sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-if Rails.env.production?
-  Sentry.init do |config|
-    config.dsn = 'https://129c7bee400f4967828bee2b066b57f5@o522223.ingest.sentry.io/6002256'
-    config.breadcrumbs_logger = %i[active_support_logger http_logger]
-
-    # config.traces_sample_rate = 1.0
-    config.traces_sampler = ->(_context) { true }
-  end
-end
+# if Rails.env.production?
+#   Sentry.init do |config|
+#     config.dsn = 'https://129c7bee400f4967828bee2b066b57f5@o522223.ingest.sentry.io/6002256'
+#     config.breadcrumbs_logger = %i[active_support_logger http_logger]
+#
+#     # config.traces_sample_rate = 1.0
+#     config.traces_sampler = ->(_context) { true }
+#   end
+# end
