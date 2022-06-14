595dbd55822bc21661f5bd2323155831509a4781
Mike Hall <mike@just3ws.com>
2022-06-14 09:54:25 -0500

    RE

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount RailsEventStore::Browser => '/res' if Rails.env.development?
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
