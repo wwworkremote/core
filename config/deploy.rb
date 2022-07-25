9d164b2d82faa474eb553aa4b4888890f7a13488
Mike Hall <mike@just3ws.com>
2022-07-25 18:31:15 -0500

    cap stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,3 +19,30 @@ set :ssh_options, { forward_agent: true }
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
 # append :linked_files, 'config/database.yml', 'config/secrets.yml'
+
+namespace :puma do
+  desc 'Create directories for Puma PIDs and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir -p #{shared_path}/tmp/sockets"
+      execute "mkdir -p #{shared_path}/tmp/pids"
+    end
+  end
+
+  before 'deploy:starting', 'puma:make_dirs'
+end
+
+namespace :deploy do
+  desc 'Ensure git is in sync with remote'
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/main`
+        puts 'WARNING: HEAD is not the same as origin/main'
+        puts 'Run `git push` to sync changes.'
+
+        exit
+      end
+    end
+  end
+end
+
