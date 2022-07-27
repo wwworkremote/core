44a3a8a10a60b3e7c0487c8f51d26cf693ce85f3
Mike Hall <mike@just3ws.com>
2022-07-27 18:18:10 -0500

    Puma

---
+++ config/environments/production/puma.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env puma
+# frozen_string_literal: true
+
+environment 'production'
+
+app_dir = '/home/deploy/projects/wwworkremote/core/current'
+shared_dir = '/home/deploy/projects/wwworkremote/core/shared'
+
+bind "unix://#{shared_dir}/sockets/puma.sock"
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
+directory "#{app_dir}/"
+
+stdout_redirect "#{shared_dir}/log/puma.access.log", "#{shared_dir}/log/puma.error.log", true
+
+workers 2
+threads 1, 2
+
+activate_control_app "unix://#{app_dir}/pumactl.sock"
+
+# prune_bundler
