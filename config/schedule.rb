0c676172edce6b4fd157a738e191bc7994e8f69e
Mike Hall <mike@just3ws.com>
2022-07-27 13:01:05 -0500

    Schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,8 +4,8 @@ require 'whenever'
 
 env :PATH, ENV.fetch('PATH', nil)
 
-# set :job_template, "/usr/bin/env bash -l -c ':job' "
-set :job_template, nil
+set :job_template, "/usr/bin/env bash -l -c ':job' "
+# set :job_template, nil
 set :output, '/home/deploy/projects/wwworkremote/core/shared/log/cron_production.log'
 
 JOB_PREFIX = ' cd :path && :environment_variable=:environment nice -n 20 '
@@ -15,4 +15,5 @@ job_type :rails, " #{JOB_PREFIX} bundle exec rails :task --silent :output "
 job_type :runner, " #{JOB_PREFIX} bundle exec rails runner :task :output "
 job_type :script, " #{JOB_PREFIX} bundle exec bin/:task :output "
 
-every(5.minutes) { runner('HeartbeatWorker.perform_async') }
+every(1.minute) { runner('Rails.logger.info("Hi | #{Time.zone.now}")') }
+every(1.minute) { runner('HeartbeatWorker.perform_async') }
