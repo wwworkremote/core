e8bb0749f397354eb896590bef11b227740581ea
Mike Hall <mike@just3ws.com>
2022-07-27 20:06:47 -0500

    Deploy with RES

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount RailsEventStore::Browser => '/res' if Rails.env.development?
+  mount RailsEventStore::Browser => '/res' # if Rails.env.development?
 
   resources :nodes, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
