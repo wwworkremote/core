82d6b9f14ec996fa271ecf51560ec7b371c1a2ff
Mike Hall <mike@just3ws.com>
2022-07-28 17:55:21 -0500

    Adding initial job_boards storage

---
+++ db/migrate/20220728223525_create_job_boards_queries.rb
@@ -0,0 +1,12 @@
+class CreateJobBoardsQueries < ActiveRecord::Migration[7.0]
+  def change
+    create_table :job_boards_queries do |t|
+      t.integer :source_id, null: false
+      t.jsonb :data, default: {}, null: false
+      t.string :aasm_state
+
+      t.datetime 'created_at', precision: 6, default: -> { 'CURRENT_TIMESTAMP' }, null: false
+      t.datetime 'updated_at', precision: 6, default: -> { 'CURRENT_TIMESTAMP' }, null: false
+    end
+  end
+end
---
+++ db/migrate/20220728223839_create_job_boards_sources.rb
@@ -0,0 +1,16 @@
+class CreateJobBoardsSources < ActiveRecord::Migration[7.0]
+  def change
+    create_table(:job_boards_sources) do |t|
+      t.string :name, null: false
+      t.string :slug
+
+      t.jsonb :data, default: {}, null: false
+      t.string :aasm_state
+
+      t.datetime 'created_at', precision: 6, default: -> { 'CURRENT_TIMESTAMP' }, null: false
+      t.datetime 'updated_at', precision: 6, default: -> { 'CURRENT_TIMESTAMP' }, null: false
+    end
+
+    add_index :job_boards_sources, :slug, unique: true
+  end
+end
