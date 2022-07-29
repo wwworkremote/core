2c7489e23862a751ce1f9b99d1bea595c370a77e
Mike Hall <mike@just3ws.com>
2022-07-29 08:34:05 -0500

    Asdf

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '~> 3.17.0'
 
 set :application, 'core'
 set :repo_url, 'git@github.com:wwworkremote/core.git'
-set :asdf_tools, %w[ruby]
+set :asdf_tools, %w[ruby nodejs golang python]
 set :branch, 'main'
 set :deploy_to, '/home/deploy/projects/wwworkremote/core'
 
