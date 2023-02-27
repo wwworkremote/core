ef40484ef084af06aa9ba7147896d9a3d7266f57
Mike Hall <mike@just3ws.com>
2023-02-26 21:34:16 -0600

    Users'

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,5 +15,9 @@
 #  index_users_on_slug   (slug) UNIQUE
 #
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :messages
 end
