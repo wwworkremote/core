25b49402d6e08438bb3998daf8eeb6031a0102c7
Mike Hall <mike@just3ws.com>
2023-02-26 20:41:05 -0600

    Annotate

--- app/models/job_boards/query.rb
+++ app/models/job_boards/query.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: job_boards_queries
+#
+#  id         :bigint           not null, primary key
+#  aasm_state :string
+#  data       :jsonb            not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  source_id  :integer          not null
+#
 class JobBoards::Query < ApplicationRecord
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :bigint           not null, primary key
+#  body       :string           not null
+#  subject    :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :uuid             not null
+#
+# Indexes
+#
+#  index_messages_on_subject  (subject)
+#  index_messages_on_user_id  (user_id)
+#
 class Message < ApplicationRecord
   belongs_to :user
 end
