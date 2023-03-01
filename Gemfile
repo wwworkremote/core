c2885d8784dd4a31661cbb4556016e3df550f125
Mike Hall <mike@just3ws.com>
2023-03-01 17:31:56 -0600

    Rails Admin

--- Gemfile
+++ Gemfile
@@ -61,3 +61,6 @@ group :development do
   gem 'rubocop-rubycw', require: false
   gem 'rubocop-thread_safety', require: false
 end
+
+gem 'rails_admin', '~> 3.1'
+gem 'sassc-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :users
   resources :messages
