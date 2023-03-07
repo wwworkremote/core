2f3b8655bd84bdfbffa7b7c6a46ddd0a44b13fd6
Mike Hall <mike@just3ws.com>
2023-03-07 16:38:50 -0600

    Cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -16,4 +16,4 @@ job_type :script, " #{JOB_PREFIX} bin/:task :output "
 
 every(15.minutes) { runner('HeartbeatWorker.perform_async') }
 
-every(3.hours) { rails('hacker_news:fetch_latest_jobstories') }
+every(9.hours) { rails('hacker_news:fetch_latest_jobstories') }
