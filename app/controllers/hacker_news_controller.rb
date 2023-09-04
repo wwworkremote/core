760632ca49e6bd94ef2594a2d82a5e5f55856525
Mike Hall <mike@just3ws.com>
2023-09-04 12:37:46 -0500

    Improve endpoitn

--- app/controllers/hacker_news_controller.rb
+++ app/controllers/hacker_news_controller.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 class HackerNewsController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def fetch_jobstory
     jobstory_id = parameters[:jobstory_id]
-    wait_until = Integer(parameters[:wait_until]).seconds
+    wait_until = parameters[:wait_until]
 
-    Rails.logger.info { "#{self.class.name}##{__method__} ==>> jobstory_id:#{jobstory_id}" }
+    Rails.logger.info { "#{self.class.name}##{__method__} ==>> jobstory_id:#{jobstory_id}, wait_until:#{wait_until}" }
+
     HackerNews::FetchJobstoryWorker
       .set(wait_until:)
       .perform_async(jobstory_id)
