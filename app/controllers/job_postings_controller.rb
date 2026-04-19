a18fcce326aafe869003df45c8849f80b530426b
Mike Hall <mike@just3ws.com>
2026-04-19 17:03:45 -0500

    fix(controllers): resolve SyntaxError in JobPostingsController#show

--- app/controllers/job_postings_controller.rb
+++ app/controllers/job_postings_controller.rb
@@ -14,10 +14,9 @@ class JobPostingsController < ApplicationController
 
     @job_postings = @job_postings.page(params[:page]).per(20)
   end
-def show
-  @job_posting = JobPosting.includes(:source, :pipeline_steps, :contacts, versions: :item).find(params[:id])
-end
 
+  def show
+    @job_posting = JobPosting.includes(:source, :pipeline_steps, :contacts, versions: :item).find(params[:id])
     ahoy.track "Viewed Job Posting", job_posting_id: @job_posting.id, title: @job_posting.title
   end
 end
