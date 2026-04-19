a5a4d37dbb299315f962fe2ce763726eb7d9a92b
Mike Hall <mike@just3ws.com>
2026-04-18 20:15:56 -0500

    fix(jobs): force Mission Control interface into AsyncJobAdapter
    - Directly reopen class to ensure methods like #supported_job_statuses are present
    - Fix persistent NoMethodError in Mission Control dashboard

--- config/initializers/async_job_adapter.rb
+++ config/initializers/async_job_adapter.rb
@@ -4,54 +4,54 @@ require 'async/job/processor/inline'
 
 # Mission Control Jobs expects the adapter to respond to #activating
 # and other querying methods like #queues.
-module AsyncJobAdapterActivatingPatch
-  def activating(&)
-    yield
-  end
+module ActiveJob
+  module QueueAdapters
+    class AsyncJobAdapter
+      def activating(&)
+        yield
+      end
 
-  def queues
-    []
-  end
+      def queues
+        []
+      end
 
-  def jobs
-    []
-  end
+      def jobs
+        []
+      end
 
-  def find_job(job_id, *)
-    nil
-  end
+      def find_job(job_id, *)
+        nil
+      end
 
-  def finished_jobs
-    []
-  end
+      def finished_jobs
+        []
+      end
 
-  def failed_jobs
-    []
-  end
+      def failed_jobs
+        []
+      end
 
-  def scheduled_jobs
-    []
-  end
+      def scheduled_jobs
+        []
+      end
 
-  def paused_queues
-    []
-  end
+      def paused_queues
+        []
+      end
 
-  def supported_job_statuses
-    []
-  end
+      def supported_job_statuses
+        []
+      end
 
-  def supports_filtering?
-    false
-  end
+      def supports_filtering?
+        false
+      end
 
-  def recurring_tasks
-    []
+      def recurring_tasks
+        []
+      end
+    end
   end
-end
-
-ActiveSupport.on_load(:active_job) do
-  ActiveJob::QueueAdapters::AsyncJobAdapter.include(AsyncJobAdapterActivatingPatch)
 end
 
 Rails.application.configure do
