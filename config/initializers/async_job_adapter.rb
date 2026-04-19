5ea80b0308796111f0f3e30dcef6a1732a63b113
Mike Hall <mike@just3ws.com>
2026-04-18 19:02:19 -0500

    fix(jobs): resolve MissionControl::Jobs crash with AsyncJob adapter
    - Implement #activating on AsyncJobAdapter to satisfy Mission Control Jobs requirement
    - Use ActiveSupport.on_load(:active_job) for clean monkey-patch integration

--- config/initializers/async_job_adapter.rb
+++ config/initializers/async_job_adapter.rb
@@ -2,6 +2,18 @@
 
 require 'async/job/processor/inline'
 
+# Mission Control Jobs expects the adapter to respond to #activating
+# to wrap block execution within the adapter's context.
+module AsyncJobAdapterActivatingPatch
+  def activating(&)
+    yield
+  end
+end
+
+ActiveSupport.on_load(:active_job) do
+  ActiveJob::QueueAdapters::AsyncJobAdapter.include(AsyncJobAdapterActivatingPatch)
+end
+
 Rails.application.configure do
   config.async_job.define_queue 'default' do
     dequeue Async::Job::Processor::Inline
