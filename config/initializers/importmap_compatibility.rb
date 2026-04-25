774cac6e01388fb83919883a83a3012dea9e6402
Mike Hall <mike@just3ws.com>
2026-04-25 07:52:14 -0500

    Fix: ensure importmap shim tags are available in ActionView

--- config/initializers/importmap_compatibility.rb
+++ config/initializers/importmap_compatibility.rb
@@ -2,12 +2,16 @@
 
 # Compatibility layer for importmap-rails 2.x
 # ahoy_captain 1.2.0 expects shim tags that were removed in importmap-rails 2.0
-ActiveSupport.on_load(:action_view) do
-  def javascript_importmap_shim_tag
-    nil
-  end
 
-  def javascript_importmap_shim_nonce_configuration_tag
-    nil
-  end
+# Define them globally in ActionView::Base to ensure they are available everywhere
+ActiveSupport.on_load(:action_view) do
+  include(Module.new {
+    def javascript_importmap_shim_tag
+      nil
+    end
+
+    def javascript_importmap_shim_nonce_configuration_tag
+      nil
+    end
+  })
 end
