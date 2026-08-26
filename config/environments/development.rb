0ab335cf60c6b75ae6d3ced8b42b191d952816bd
Mike Hall <mike@just3ws.com>
2026-08-26 12:42:19 -0500

    Fix(lan): allow short home hostname

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
 
   # Rails rejects non-local Host headers by default. These names are served
   # only on the trusted LAN through the local AdGuard rewrites.
-  config.hosts = ["localhost", "127.0.0.1", "::1", "wwworkremote.home.arpa", "wwr.home.arpa"]
+  config.hosts = ["localhost", "127.0.0.1", "::1", "wwworkremote.home.arpa", "wwwr.home.arpa", "wwr.home.arpa"]
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
